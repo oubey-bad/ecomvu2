373650dc25977c100748623b8305241f859bdd4d
oubey-bad <oubeybadis20@gmail.com>
2024-10-17 14:06:57 +0100

    color and size controller

--- app/Http/Controllers/Api/Admin/ProductController.php
+++ app/Http/Controllers/Api/Admin/ProductController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\admin;
+namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
---
+++ database/migrations/2024_10_16_160235_create_colors_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('colors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Color name (e.g., Red, Blue)
+            $table->string('hex_code')->nullable(); // Hex code for the color (e.g., #FF0000)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('colors');
+    }
+};
